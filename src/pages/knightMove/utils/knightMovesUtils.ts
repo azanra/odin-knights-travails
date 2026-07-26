2f53b049b2db8af314ad45fc3a0ceaa71d6ac42f
azan-personal <112457939+azanra@users.noreply.github.com>
2026-07-26 08:22:39 +0700

    feat: add helper to get valid move for vertex

--- src/pages/knightMove/utils/knightMovesUtils.ts
+++ src/pages/knightMove/utils/knightMovesUtils.ts
@@ -9,22 +9,28 @@ const knightMovesUtils = (() => {
     startPosition: IPosition,
     targetPosition: IPosition,
   ) => {
-    const queue = [];
+    let queue: IPosition[] = [];
     queue.push(startPosition);
 
+    const visited: IPosition[] = [];
+
     while (queue.length !== 0) {
-      const firstItem = queue[0];
+      const firstItem = queue.shift();
       if (!firstItem) break;
 
       const possibleMove = getNextPossibleMove(firstItem);
+      const validMove = getValidMove(possibleMove, visited);
+
+      queue = [...queue, ...validMove];
+      visited.push(firstItem);
     }
   };
 
-  const getNextPossibleMove = (startPosition: IPosition) => {
+  const getNextPossibleMove = (startPosition: IPosition): IPosition[] => {
     return NEXT_MOVE.map((move) => [...getNextMove(startPosition, move)]);
   };
 
-  const getNextMove = (position: IPosition, nextMove: INextMove) => {
+  const getNextMove = (position: IPosition, nextMove: INextMove): IPosition => {
     const [x, y] = position;
 
     return [
@@ -39,6 +45,24 @@ const knightMovesUtils = (() => {
     amount: number,
   ) => {
     return operator === "+" ? axesPosition + amount : axesPosition - amount;
+  };
+
+  const getValidMove = (possibleMove: IPosition[], visited: IPosition[]) => {
+    return possibleMove.filter((move) => {
+      const [x, y] = move;
+      return isValid(x) && isValid(y) && !isVisited(move, visited);
+    });
+  };
+
+  const isValid = (axes: number) => axes >= 0 && axes <= 7;
+
+  const isVisited = (move: IPosition, visited: IPosition[]) => {
+    const stringifiedVisited = JSON.stringify(visited);
+    const stringifiedMove = JSON.stringify(move);
+
+    const indexOccurrence = stringifiedVisited.indexOf(stringifiedMove);
+
+    return indexOccurrence !== -1;
   };
 
   return { levelOrderTraversal };
